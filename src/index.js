9a9291b307a22f2aef2a6f0069c94b7f038601e4
Philip Mes <fmesy@outlook.com>
2018-09-09 00:11:40 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+// import App from './App';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './components/app';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+ReactDOM.render(
+  <Provider store={createStoreWithMiddleware(reducers)}>
+    <App />
+  </Provider>
+  , document.getElementById('root')
+);
+registerServiceWorker();
